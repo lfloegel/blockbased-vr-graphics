103bab1261b4a2fdc038bbbc16e98e6bb3a96e93
Lara FS <lfloegelshetty@cs.ucsb.edu>
2018-03-01 17:15:52 -0800

    graphics'

---
+++ Assets/Scripts/Blocks/CodeBuilder.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+function Start () {
+	//GetComponent(Codeblock).sayHi();
+
+}
+
+function Update () {
+
+}
+
+function createBlockObj(){
+	//Create block model
+	//<insert code here>
+
+	//New block
+	Debug.Log("Block object created");
+	GetComponent(Codeblock).sayHi();
+}
